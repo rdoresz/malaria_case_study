d77f63be33204a5f69d42ba4d44210d57d222cd5
rdoresz <68820705+rdoresz@users.noreply.github.com>
2022-03-04 14:32:50 +0100

    Update q_3_gc.py

--- q_3_gc.py
+++ q_3_gc.py
@@ -13,16 +13,23 @@ The gc % in the genome file will be printed on the standard output.
 
 
 import sys
-
+# setting up empty string for saving sequence lines
 nucleotide_line = ''
+# setting up empty string to calculate gc content
 gc = ''
-
+# opening input file
 with open(sys.argv[1], 'r') as fin:
+    # iterating through the file
     for line in fin:
         line = line.rstrip()
+        # passing the header lines
         if line.startswith('>'):
             continue
+        # saving the sequence lines    
         else:
+            # converting from multi line to single line
             nucleotide_line += line.lower()
+    # calculating gc content        
     gc = ((nucleotide_line.count('c')+nucleotide_line.count('g'))/len(nucleotide_line))*100
+    # printing to standard output
     print(gc)
